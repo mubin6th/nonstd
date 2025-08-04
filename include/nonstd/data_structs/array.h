cdaaf599be03f59525b50656265e570ba087bce8
mubin6th <mubin.muhammad.the.7th@gmail.com>
2025-08-04 18:14:42 +0600

    data_structs/array.h: renamed macro to shorter name. added nonstd_array_init_sv_M().

--- include/nonstd/data_structs/array.h
+++ include/nonstd/data_structs/array.h
@@ -3,17 +3,26 @@
 
 #include <stddef.h>
 
-#define nonstd_array_init_with_values_M(type, ...) \
-    (nonstd_array_t){ \
-        .ptr = (type[]){__VA_ARGS__}, \
-        .size = sizeof((type[]){__VA_ARGS__}) / sizeof(type) \
-    }
-
 #define nonstd_array_init_M(type, size) \
     (nonstd_array_t){ \
         .ptr = (type[size]){}, \
         .size = size \
     }
+
+#define nonstd_array_init_v_M(type, ...) \
+    (nonstd_array_t){ \
+        .ptr = (type[]){__VA_ARGS__}, \
+        .size = sizeof((type[]){__VA_ARGS__}) / sizeof(type) \
+    }
+
+#define nonstd_array_init_sv_M(type, size, ...) \
+    (nonstd_array_t){ \
+        .ptr = (type[size]){__VA_ARGS__}, \
+        .size = size \
+    }
+
+#define nonstd_array_at_M(array, type, idx) \
+    ((type*)array.ptr)[idx]
 
 typedef struct nonstd_array_t {
     void *ptr;
